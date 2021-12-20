cb2f87d205c8aea25680f70cabf22a38c92ce4e3
chenhui0923 <251387518@qq.com>
2021-12-20 20:55:40 +0800

    TCP/IP

--- src/main/java/com/example/demo/Iostart/TcpipTest2/Server1.java
+++ src/main/java/com/example/demo/Iostart/TcpipTest2/Server1.java
@@ -1,56 +1,72 @@
 package com.example.demo.Iostart.TcpipTest2;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server1 {
-    //监听端口
+    public static final int port = 8080;//监听的端口号
 
-    private static final int PORT = 60020;
-
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
-
-        Socket socket = null;
-
-        try {
-//建立服务器的Socket，并设定一个监听的端口PORT
-
-            serverSocket = new ServerSocket(PORT);
-
-//由于需要进行循环监听，因此获取消息的操作应放在一个while大循环中
-
-            while(true){
-                try {
-//建立跟客户端的连接
-
-                    socket = serverSocket.accept();
-
-                } catch (Exception e) {
-                    System.out.println("建立与客户端的连接出现异常");
-
-                    e.printStackTrace();
-
-                }
-
-                ServerThread thread = new ServerThread(socket);
-
-                thread.start();
-
-            }
-
-        } catch (Exception e) {
-            System.out.println("端口被占用");
-
-            e.printStackTrace();
-
-        }
-
-        finally {
-            serverSocket.close();
-
-        }
-
+    public static void main(String[] args) {
+        System.out.println("Server...\n");
+        Server1 server = new Server1();
+        server.init();
     }
 
+    public void init() {
+        try {
+            //创建一个ServerSocket，这里可以指定连接请求的队列长度
+            //new ServerSocket(port,3);意味着当队列中有3个连接请求是，如果Client再请求连接，就会被Server拒绝
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                //从请求队列中取出一个连接
+                Socket client = serverSocket.accept();
+                // 处理这次连接
+                new HandlerThread(client);
+            }
+        } catch (Exception e) {
+            System.out.println("服务器异常: " + e.getMessage());
+        }
+    }
+
+    private class HandlerThread implements Runnable {
+        private Socket socket;
+        public HandlerThread(Socket client) {
+            socket = client;
+            new Thread(this).start();
+        }
+
+        public void run() {
+            try {
+                // 读取客户端数据
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String clientInputStr = input.readLine();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
+                // 处理客户端数据
+                System.out.println("客户端发过来的内容:" + clientInputStr);
+
+                // 向客户端回复信息
+                PrintStream out = new PrintStream(socket.getOutputStream());
+                System.out.print("请输入:\t");
+                // 发送键盘输入的一行
+                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                out.println(s);
+
+                out.close();
+                input.close();
+            } catch (Exception e) {
+                System.out.println("服务器 run 异常: " + e.getMessage());
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (Exception e) {
+                        socket = null;
+                        System.out.println("服务端 finally 异常:" + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
 }
+
